8b6a7a995aee8ae395feab316134a0811f948ff1
Bobby <bobbykumar706584@gmail.com>
2021-08-12 19:22:46 +0530

    learn nextjs

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = ()=> {
+	return(
+		<footer>
+			Copyright 2021 Ninja list
+		</footer>
+	)
+}
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis sapien eros, quis consequat ligula dignissim nec. Maecenas sit amet cursus purus. Praesent semper porta tellus sit amet scelerisque. Nulla eros nibh, congue sed odio vel, consequat pharetra ligula. In a tincidunt dui. Integer at orci lacus. Fusce at laoreet ipsum. Sed vitae tellus lacus. Cras malesuada neque ac orci dignissim, finibus tincidunt odio accumsan. Ut pellentesque eros interdum urna auctor euismod.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis sapien eros, quis consequat ligula dignissim nec. Maecenas sit amet cursus purus. Praesent semper porta tellus sit amet scelerisque. Nulla eros nibh, congue sed odio vel, consequat pharetra ligula. In a tincidunt dui. Integer at orci lacus. Fusce at laoreet ipsum. Sed vitae tellus lacus. Cras malesuada neque ac orci dignissim, finibus tincidunt odio accumsan. Ut pellentesque eros interdum urna auctor euismod.</p>
+		</div>
+	)
+}
+
+export default About;
